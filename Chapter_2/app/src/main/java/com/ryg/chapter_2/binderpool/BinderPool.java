d9a4e29a9a2320cdb8785664d96d709973b6bd43
ZhangKuixun <ZhangKuixun@gmail.com>
2019-08-27 11:19:15 +0800

    frist

---
+++ Chapter_2/app/src/main/java/com/ryg/chapter_2/binderpool/BinderPool.java
@@ -0,0 +1,128 @@
+package com.ryg.chapter_2.binderpool;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+
+import java.util.concurrent.CountDownLatch;
+
+public class BinderPool {
+    private static final String TAG = "BinderPool";
+    public static final int BINDER_NONE = -1;
+    public static final int BINDER_COMPUTE = 0;
+    public static final int BINDER_SECURITY_CENTER = 1;
+
+    private Context mContext;
+    private IBinderPool mBinderPool;
+    private static volatile BinderPool sInstance;
+    private CountDownLatch mConnectBinderPoolCountDownLatch;
+
+    private BinderPool(Context context) {
+        mContext = context.getApplicationContext();
+        connectBinderPoolService();
+    }
+
+    public static BinderPool getInsance(Context context) {
+        if (sInstance == null) {
+            synchronized (BinderPool.class) {
+                if (sInstance == null) {
+                    sInstance = new BinderPool(context);
+                }
+            }
+        }
+        return sInstance;
+    }
+
+    private synchronized void connectBinderPoolService() {
+        mConnectBinderPoolCountDownLatch = new CountDownLatch(1);
+        Intent service = new Intent(mContext, BinderPoolService.class);
+        mContext.bindService(service, mBinderPoolConnection, Context.BIND_AUTO_CREATE);
+        try {
+            //CountDownLatch 将异步转成同步，也就是说，bindService是异步的，我们通过CountDownLatch把它转成同步。
+            mConnectBinderPoolCountDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * query binder by binderCode from binder pool
+     *
+     * @param binderCode the unique token of binder
+     * @return binder who's token is binderCode<br>
+     * return null when not found or BinderPoolService died.
+     */
+    public IBinder queryBinder(int binderCode) {
+        IBinder binder = null;
+        try {
+            if (mBinderPool != null) {
+                binder = mBinderPool.queryBinder(binderCode);
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return binder;
+    }
+
+    private ServiceConnection mBinderPoolConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            // ignored.
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mBinderPool = IBinderPool.Stub.asInterface(service);
+            try {
+                //当service段了以后，binder dea。如果说远程service被干掉了，可以断线重连。
+                mBinderPool.asBinder().linkToDeath(mBinderPoolDeathRecipient, 0);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+            mConnectBinderPoolCountDownLatch.countDown();
+        }
+    };
+
+    //重新去绑定连接远程service
+    private IBinder.DeathRecipient mBinderPoolDeathRecipient = new IBinder.DeathRecipient() {
+        @Override
+        public void binderDied() {
+            Log.w(TAG, "binder died.");
+            mBinderPool.asBinder().unlinkToDeath(mBinderPoolDeathRecipient, 0);
+            mBinderPool = null;
+            connectBinderPoolService();
+        }
+    };
+
+    public static class BinderPoolImpl extends IBinderPool.Stub {
+
+        public BinderPoolImpl() {
+            super();
+        }
+
+        @Override
+        public IBinder queryBinder(int binderCode) throws RemoteException {
+            IBinder binder = null;
+            switch (binderCode) {
+                case BINDER_SECURITY_CENTER: {
+                    binder = new SecurityCenterImpl();
+                    break;
+                }
+                case BINDER_COMPUTE: {
+                    binder = new ComputeImpl();
+                    break;
+                }
+                default:
+                    break;
+            }
+
+            return binder;
+        }
+    }
+
+}
